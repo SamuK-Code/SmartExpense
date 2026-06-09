dd57783245c5147a565e450311f2f6aa7293ff9c
SamuK-Code <samuel30nelk@gmail.com>
2026-06-09 08:32:24 -0300

    Update I18nContext.js

--- src/context/I18nContext.js
+++ src/context/I18nContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useCallback } from 'react';
+import React, { createContext, useContext, useState, useEffect, useCallback } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const I18nContext = createContext();
